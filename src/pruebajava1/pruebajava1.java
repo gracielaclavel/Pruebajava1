2c34f709fdd9ded7a11b400c03dac5c33451ae22
gracielaclavel <gracy63@gmail.com>
2017-10-30 19:13:12 -0300

    dando responsabilidades  de  clase principal

---
+++ src/pruebajava1/pruebajava1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pruebajava1;
+
+
+/********************************************************************************
+ *Graciela  Clavel 
+ * @author lp12
+ *Fecha: Octubre 30 de  2017
+ * Preograma: pruebajava1
+ * Responsabilidad: Clase Principal
+ *******************************************************************************/
+
+public class pruebajava1 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
+    
